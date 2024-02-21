0c2b97366c65381b34f21515ad5aec32a49c4f8c
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-02-20 22:31:15 -0300

    Aula 20/02

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ let altura = 1.75;
 console.log('Nome: ' + nome + ' - Idade: ' + idade + ' anos');
 console.log(`Nome: ${nome} - Idade: ${idade} anos`);
 
-//Variavel Booleana
+//Variavel que recebe um valor Booleano
 let trabalha = true;
 console.log(trabalha);
 
-//Ambos fazem o mesmo resultado
+//Ambos fazem o mesmo resultado. Aqui trabalhamos com condicionais e concatenação//
 console.log('Trabalha: ' + (trabalha === true ? 'Sim' : 'Não'));
 if (trabalha === true){
     console.log('Trabalha: Sim');
@@ -28,25 +28,39 @@ console.log(
     'vNome: ' + typeof(nome) + '\n' +
     'vIdade: ' + typeof(idade) + '\n' +
     'vAltura: ' + typeof(altura) + '\n' +
-    'vTrabalha: ' + typeof(trabalha) + '\n' +
-    'vEndereco: ' + typeof(endereco) + '\n' +
-    'vFrutas: ' + typeof(frutas)
+    'vTrabalha: ' + typeof(trabalha)
  );
 
- let endereco = (
+ //Variavel do tipo Objeto
+ let endereco = {
     'rua' : 'Brasil',
     'num' : 311,
     'bairro' : 'centro',
     'cidade' : 'bastos',
     'uf' : 'SP',
-    'mora' : true
- );
+    'mora ': true
+ };
 
+ console.log(endereco);
+ console.log(typeof(endereco));
+
+ //Variavel do tipo Array/Lista
  let frutas = ['banana', 'maçã', ' mamão'];
+ console.log(frutas[0]);
 
+ //Variavel do tipo Array/Lista em Duas dimensões
  let velha = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8]
  ];
  console.log(velha [2][2]);
+ console.log(typeof(velha));
+
+ //Array.isArray ele expressa uma resposta booleana [..]
+ console.log(Array.isArray(velha));
+
+ /*[..] Quando há uma condicional escrita em String ele 
+ devolverá em String ao invés de Booleana*/
+ console.log((Array.isArray(velha)) ? 'Array' : 'Não é array');
+ console.log((Array.isArray(endereco)) ? 'Array' : 'Não é array');
