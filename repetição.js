d6c9616a27855091f14f033e2e4be4d4c5f6ca53
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-02-22 20:38:41 -0300

    Aula 22/02 pt. 2

--- repetição.js
+++ repetição.js
@@ -1,11 +1,49 @@
 let entrada = require ('prompt-sync')();
 
-let idade = entrada('Digite sua idade: ');
+/*let idade = entrada('Digite sua idade: ');
+
+let idadeInt = parseInt(idade);
 
 if (idade < 18) {
     console.log('Menor');
-} else if (idade == 18){
+} else if (idade === 18){
     console.log('Tem 18');
 } else {
     console.log('Maior');
+}
+
+console.log(`Sua idade é ${idade}`);*/
+
+/* O prompt-sync ele permite que possamos usar o comando 'entrada =',
+um pacote npm através do 'require()'.
+
+== ou Ele ignora o "tipo" da variavel, já o === ele
+considera até mesmo o tipo da variavel. Como o 18 não 
+está entre aspas ele é number e não String como está na
+resposta da condicional.
+
+O let idadeInt = parseInt(idade); --> Nada mais é que
+transformar a variavel 'idade' em Inteiro, possibilitando
+que ele seja Number ao inves de String.
+
+parseFloat ele transformará para decimais.
+
+*/
+
+
+let num = parseInt (entrada('Digite um número para visualizar a tabuada!'))
+
+let cont = 1;
+
+/*
+while(cont <= 10){
+    let calc = num * cont;
+    console.log(`${num} X ${cont} = ${calc}`);
+    cont++;
+}
+*/
+
+for (let cont =1; cont<=10; cont++){
+    let calc = num * cont;
+    console.log(`${num} X ${cont} = ${calc}`);
 }
